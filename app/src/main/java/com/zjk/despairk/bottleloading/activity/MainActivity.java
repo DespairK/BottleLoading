985810817cdd3ed86e70a6f92207035299e836ed
DespairK <15010209351@163.com>
2017-01-05 11:02:34 +0800

    first

---
+++ app/src/main/java/com/zjk/despairk/bottleloading/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zjk.despairk.bottleloading.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zjk.despairk.bottleloading.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
